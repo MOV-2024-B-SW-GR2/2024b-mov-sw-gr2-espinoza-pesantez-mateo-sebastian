a56188c5f5b311720419df537ec625e33930bee5
Theo-17 <mateo.espinoza@epn.edu.ec>
2024-10-24 08:50:12 -0500

    class 4

---
+++ 00-Kotlin/kotlin/src/main/kotlin/Main.kt
@@ -0,0 +1,19 @@
+package com.ejemplo.redsocial
+
+import java.util.Date
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    val name = "Kotlin"
+    val estado = "C";
+    val fechaNacimiento: Date = Date();
+
+    when (estado) {
+        ("C") -> {
+            println("CASADO");
+        }
+    }
+
+
+}
